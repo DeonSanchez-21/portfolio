af92ec1ac0523346dea2f933d1181ca119cd4c54
Deon <dsanchez6@gmail.com>
2023-03-07 17:04:52 -0500

    added project attributes

--- index.js
+++ index.js
@@ -45,3 +45,43 @@ const observer = new IntersectionObserver(entries => {
 })
 
 observer.observe(name)
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "deonsanchez6@gmail.com",
+        Password : "706EFF27EBCFD328815416D07F52318008B5",
+        To : 'deonsanchez6@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "new contact form inquery",
+        Body : `Name: ${document.getElementById("first-name").value} ${document.getElementById("last-name").value}<br>
+        Number: ${document.getElementById("phone-number").value}<br>
+        Message: ${document.getElementById("message").value}`,
+    }).then(message => alert(message)
+    );
+} 
+
+//animations
+const homeSection = document.querySelector('.home-section');
+const sections = document.querySelectorAll(".sections");
+const sliders = document.querySelectorAll(".animate");
+
+const appearOptions = {
+    
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){
+            return
+        } else {
+            entry.target.classList.add("slide");
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions);
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider)
+})
+
