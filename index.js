80d37f0ce3598f7a30b81506443ec39f8667764f
Deon <dsanchez6@gmail.com>
2023-04-17 15:43:35 -0400

    added logo

--- index.js
+++ index.js
@@ -47,18 +47,6 @@ const observer = new IntersectionObserver(entries => {
 observer.observe(name)
 
 function sendEmail(){
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "deonsanchez6@gmail.com",
-        Password : "706EFF27EBCFD328815416D07F52318008B5",
-        To : 'deonsanchez6@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "new contact form inquery",
-        Body : `Name: ${document.getElementById("first-name").value} ${document.getElementById("last-name").value}<br>
-        Number: ${document.getElementById("phone-number").value}<br>
-        Message: ${document.getElementById("message").value}`,
-    }).then(message => alert(message)
-    );
 } 
 
 //animations
